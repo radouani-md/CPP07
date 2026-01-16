73a8e1012391fdcaf24aeb7c866279064b22910d
Mohammed Radouani <mradouan@e1r3p8.1337.ma>
2026-01-16 16:12:18 +0100

    in ex01

---
+++ ex00/whatever.hpp
@@ -0,0 +1,32 @@
+#ifndef __WHATEVER_HPP__ 
+#define __WHATEVER_HPP__
+
+#include <iostream>
+
+template <typename T>
+void    swap(T& a, T& b)
+{
+    T c;
+    c = a;
+    a = b;
+    b = c;
+}
+template <typename T>
+T    max(T a, T b)
+{
+    if (a > b)
+        return a;
+    else
+        return b;
+}
+template <typename T>
+T    min(T a, T b)
+{
+     if (a < b)
+        return a;
+    else
+        return b;
+}
+
+
+#endif
